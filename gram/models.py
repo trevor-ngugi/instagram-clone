3bc2b6e1c46257e8e2bda19f2070a0f251609afd
trevor ngugi <ngugitrevor@gmail.com>
2020-02-09 11:53:47 +0300

    add image model

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image=models.CharField(max_length=30)
+    image_name=models.CharField(max_length=30)
+    image_caption=models.CharField(max_length=30)
+    likes=models.IntegerField()
+    comments=models.CharField(max_length=30)
