b25c59db29d9054a400c4909ae4db4a9e3db6d37
trevor ngugi <ngugitrevor@gmail.com>
2020-02-09 17:21:54 +0300

    add bstyling

--- gram/views.py
+++ gram/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from.models import Image
 
 # Create your views here.
 def home(request):
-    return render(request,'ig/home.html')
+    posts=Image.show_images()
+    return render(request,'ig/home.html',{'posts':posts})
