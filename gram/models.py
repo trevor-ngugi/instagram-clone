469bd091b16bbd4948fcf5cc5b83716737b2e20c
trevor ngugi <ngugitrevor@gmail.com>
2020-02-09 11:58:35 +0300

    add profile model

--- gram/models.py
+++ gram/models.py
@@ -7,3 +7,12 @@ class Image(models.Model):
     image_caption=models.CharField(max_length=30)
     likes=models.IntegerField()
     comments=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.image_name
+
+class Profile(models.Model):
+    profile_photo=models.CharField(max_length=30)
+    bio=models.CharField(max_length=30)
+
+    
