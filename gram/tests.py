b8407f1b74f518e2443dbd4778d876691db953fb
trevor ngugi <ngugitrevor@gmail.com>
2020-02-09 12:20:28 +0300

    add test instance

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Image,Profile
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.trevor=Profile(profile_photo='image',bio='wonderful')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.trevor,Profile))
