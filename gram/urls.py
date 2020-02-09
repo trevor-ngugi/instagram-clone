c964a2b902c359d955c7101a328f6c8e7c2de071
trevor ngugi <ngugitrevor@gmail.com>
2020-02-09 22:11:32 +0300

    add upload image feature

--- gram/urls.py
+++ gram/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
     url(r'^search/',views.search_profile,name='search_profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
