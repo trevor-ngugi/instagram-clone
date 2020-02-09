0f9a4558082a7c1a33a1e911130d7fd111041adb
trevor ngugi <ngugitrevor@gmail.com>
2020-02-09 21:05:22 +0300

    add search profile view function

--- gram/views.py
+++ gram/views.py
@@ -5,3 +5,14 @@ from.models import Image
 def home(request):
     posts=Image.show_images()
     return render(request,'ig/home.html',{'posts':posts})
+
+def search_profile(request):
+    if 'profile' in request.GET and request.GET['profile']:
+        search_term=request.GET.get('profile')
+        results_profiles=Image.search_profile('search_term')
+        message=f'{search_term}'
+        return render(request,'ig/search.html',{'message':message,'profiles':results_profiles})
+
+    else:
+        message='you havent searched for any thing'
+        return render(request,'ig/search.html',{'message':message})
