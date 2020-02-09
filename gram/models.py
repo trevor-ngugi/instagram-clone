17dbf6ddae42926a1867c27d054375daade5eb47
trevor ngugi <ngugitrevor@gmail.com>
2020-02-09 12:10:45 +0300

    add foeirgn key and time field in image model

--- gram/models.py
+++ gram/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    image=models.CharField(max_length=30)
-    image_name=models.CharField(max_length=30)
-    image_caption=models.CharField(max_length=30)
-    likes=models.IntegerField()
-    comments=models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.image_name
 
 class Profile(models.Model):
     profile_photo=models.CharField(max_length=30)
     bio=models.CharField(max_length=30)
 
-    
+
+class Image(models.Model):
+    image=models.CharField(max_length=30)
+    image_name=models.CharField(max_length=30)
+    image_caption=models.CharField(max_length=30)
+    profile=models.ForeignKey(Profile)
+    likes=models.IntegerField()
+    comments=models.CharField(max_length=30)
+    post_time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image_name
+
+
+
+
